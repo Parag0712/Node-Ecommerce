25c03695736c1a5c5a34a82ee3c7b7271da8e7ed
Parag0712 <paragvadgama123@gmail.com>
2024-03-02 23:30:55 +0530

    delete controllers added

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
-import {createUser, getAllUsers, getUser} from "../controllers/user.controllers.js";
+import {createUser, deleteUser, getAllUsers, getUser} from "../controllers/user.controllers.js";
+import { app } from "../app.js";
 
 const router  = Router();
 // Route - /api/v1/users/createUser
@@ -11,7 +12,12 @@ router.route("/getAllUsers").get(getAllUsers);
 // Route - /api/v1/users/getAllUsers
 router.route("/getAllUsers").get(getUser); 
 
-// Route - /api/v1/users/id
-router.route("/:id").get(getUser); 
+// Route - /api/v1/users/getUser/id
+router.route("/getUser/:id").get(getUser); 
+
+// Route - /api/v1/users/deleteUser/id
+router.route("/deleteUser/:id").delete(deleteUser); 
+
+// router.route("/:id").get(getUser).delete(deleteUser)
 
 export default router;
