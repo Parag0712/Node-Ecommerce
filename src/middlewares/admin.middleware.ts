8ab149d3e638a12ea2e09d26ab9897b7aa46fd52
Parag0712 <paragvadgama123@gmail.com>
2024-03-02 23:35:10 +0530

    admin middleware added

---
+++ src/middlewares/admin.middleware.ts
@@ -0,0 +1,17 @@
+
+import { User } from "../models/user.model.js";
+import { TryCatch } from "../utils/TryCatch.js";
+import ErrorHandler from "../utils/utility-class.js";
+
+// Middleware to make sure only admin is allowed
+export const adminOnly = TryCatch(async (req, res, next) => {
+    const { id } = req.query;
+
+    if (!id) return next(new ErrorHandler("user not login", 401));
+
+    const user = await User.findById(id);
+    if (!user) return next(new ErrorHandler("User Not Found", 401));
+    if (user.role !== "admin")
+        return next(new ErrorHandler("You do not have permission to access this page.", 403));
+    next();
+});
