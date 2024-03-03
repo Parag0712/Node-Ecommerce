280b25f40a60a7058d81c45f594aa46d21b1392d
Parag0712 <paragvadgama123@gmail.com>
2024-03-03 18:07:29 +0530

    SearchProduct api  added

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -1,18 +1,19 @@
-import { NewProductRequestBody } from "../types/types.js";
+import { BaseQuery, NewProductRequestBody, SearchRequestQuery } from "../types/types.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import { NextFunction, Request } from 'express';
 import ErrorHandler from "../utils/utility-class.js";
 import { Product } from "../models/product.model.js";
 import { rm } from "fs";
+// import { faker } from "@faker-js/faker";
 
 // addProduct
-export const addProduct = TryCatch(async (req: Request<{}, {}, NewProductRequestBody>, res, next:NextFunction) => {
-    const {name,price,category,stock}:NewProductRequestBody = req.body;
+export const addProduct = TryCatch(async (req: Request<{}, {}, NewProductRequestBody>, res, next: NextFunction) => {
+    const { name, price, category, stock }: NewProductRequestBody = req.body;
     const photo = req.file;
     if (!photo) return next(new ErrorHandler("Photo is Required", 400));
-    
-    if(!photo || !name || !price || !stock || !category){
-        rm(photo.path,()=>{
+
+    if (!photo || !name || !price || !stock || !category) {
+        rm(photo.path, () => {
             console.log("deleted");
         })
     }
@@ -22,65 +23,213 @@ export const addProduct = TryCatch(async (req: Request<{}, {}, NewProductRequest
     if (!stock) return next(new ErrorHandler("Stock is Required", 400));
     if (!category) return next(new ErrorHandler("Category Required", 400));
 
-    
-    const product =await Product.create({
+
+    const product = await Product.create({
         name,
         price,
-        category:category.toLowerCase(),
+        category: category.toLowerCase(),
         stock,
-        photo:photo?.path
+        photo: photo?.path
     })
 
-    if(!product){
-        rm(photo.path,()=>{
+    if (!product) {
+        rm(photo.path, () => {
             console.log("deleted");
         })
     }
 
     return res.status(201).json({
         success: true,
-        product:product,
+        product: product,
         message: "Product Added Successfully"
     })
 })
 
 // latestProduct
-export const latestProduct = TryCatch(async(req,res,next)=>{
+export const latestProduct = TryCatch(async (req, res, next) => {
 
-    const sort:any = req.query.sort || 'createdAt';
-    const order:any = req.query.order || 'desc';
-    const limit: number = req.query.limit ? parseInt(req.query.limit as string, 10) : 5; 
+    const sort: any = req.query.sort || 'createdAt';
+    const order: any = req.query.order || 'desc';
+    const limit: number = req.query.limit ? parseInt(req.query.limit as string, 10) : 5;
 
     // 1 mean asc -1 desc
-    const product = await Product.find({}).sort({[sort]:order}).limit(limit);
+    const product = await Product.find({}).sort({ [sort]: order }).limit(limit);
 
     return res.status(200).json({
         success: true,
-        product:product,
-        message: "latestProduct Fetched Successfully"
+        product: product,
+        message: "latest Products Fetched Successfully"
     })
 })
 
 // getAllCategories
-export const getAllCategories = TryCatch(async(req,res,next)=>{
+export const getAllCategories = TryCatch(async (req, res, next) => {
 
     const categories = await Product.distinct("category");
 
     return res.status(200).json({
         success: true,
-        categories:categories,
+        categories: categories,
         message: "Product Categories Fetched Successfully"
     })
 })
 
+// getSingleProduct
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const id = req.params.productId;
+    const product = await Product.findById(id);
+    if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+    return res.status(200).json({
+        success: true,
+        product: product,
+        message: "Product Fetched Successfully"
+    })
+})
+
 // getAllProduct
-export const getAllProduct = TryCatch(async(req,res,next)=>{
+export const getAllProducts = TryCatch(async (req, res, next) => {
 
     const product = await Product.find();
 
     return res.status(200).json({
         success: true,
-        product:product,
+        product: product,
         message: "Products Fetched Successfully"
     })
 })
+
+// updateProduct
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const id = req.params.productId;
+
+    const { name, price, category, stock } = req.body;
+    const photo = req.file;
+
+    const product = await Product.findById(id);
+
+    if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("deleted");
+        });
+        product.photo = photo.path;
+    }
+
+    if (name) product.name = name;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+
+    await product.save();
+
+    return res.status(201).json({
+        success: true,
+        product: product,
+        message: "Product Update Successfully"
+    })
+})
+
+// getSingleProduct
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const id = req.params.productId;
+    const product = await Product.findById(id);
+    if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+    rm(product.photo!, () => {
+        console.log("Product Photo Deleted");
+    })
+
+    await product.deleteOne();
+
+    return res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+})
+
+
+// searchProduct
+export const searchProduct = TryCatch(async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
+
+    const { category, price, search, sort } = req.query;
+
+    const page = Number(req.query.page) || 1
+
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8
+    const skip = (page - 1) * limit;
+    const baseQuery: BaseQuery = {};
+
+    if (search) {
+        baseQuery.name = {
+            $regex: search,
+            $options: "i",
+        }
+    }
+    if (price) {
+        baseQuery.price = {
+            $lte: Number(price)
+        }
+    }
+    if (category) { baseQuery.category = category }
+
+    const productPromise = Product.find(baseQuery)
+        .sort(sort ? { price: sort === "asc" ? 1 : -1 } : undefined)
+        .limit(limit)
+        .skip(skip)
+
+    const filterProductPromise = Product.find(baseQuery);
+    const [products, filterOnlyProduct] = await Promise.all([
+        productPromise,
+        filterProductPromise
+    ])
+
+    //How many page 
+    const totalPage = Math.ceil(filterOnlyProduct.length / limit);
+
+    return res.status(200).json({
+        success: true,
+        product: products,
+        totalPage,
+        message: "Products Fetched Successfully"
+    })
+})
+
+
+// const generateRandomProducts = async (count: number = 100) => {
+//     const products = [];
+
+//     for (let i = 0; i < count; i++) {
+//         const product = {
+//             name: faker.commerce.productName(),
+//             photo: "uploads\\5ba9bd91-b89c-40c2-bb8a-66703408f986.png",
+//             price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
+//             stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
+//             category: faker.commerce.department(),
+//             createdAt: new Date(faker.date.past()),
+//             updatedAt: new Date(faker.date.recent()),
+//             __v: 0,
+//         };
+
+//         products.push(product);
+//     }
+
+//     await Product.create(products);
+
+//     console.log({ succecss: true });
+// };
+// generateRandomProducts(100)
+
+
+// const deleteRandomsProducts = async (count: number = 10) => {
+//   const products = await Product.find({}).skip(2);
+
+//   for (let i = 0; i < products.length; i++) {
+//     const product = products[i];
+//     await product.deleteOne();
+//   }
+
+//   console.log({ succecss: true });
+// };
+// deleteRandomsProducts(50)
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,26 +1,36 @@
 import { Router } from "express";
-import { addProduct, getAllCategories, getAllProduct, latestProduct } from "../controllers/product.controllers.js";
+import { addProduct, deleteProduct, getAllCategories, getAllProducts, getSingleProduct, latestProduct, searchProduct, updateProduct } from "../controllers/product.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyAdmin } from "../middlewares/admin.middleware.js";
 
-const router  = Router();
+const router = Router();
 
 // => Admin routes
-// Route addProduct- /api/v1/product/addProduct
-router.route("/addProduct").post(verifyAdmin,upload,addProduct);
+//For addProduct- /api/v1/product/addProduct
+router.route("/addProduct").post(verifyAdmin,upload, addProduct);
 
-// Route getAllProduct- /api/v1/product/getAllProduct
-router.route("/getAllProducts").get(verifyAdmin,getAllProduct);
+//For getAllProduct- /api/v1/product/getAllProduct
+router.route("/getAllProducts").get(verifyAdmin, getAllProducts);
+
+//For updateProduct  - /api/v1/product/updateProduct/:productId?id
+router.put("/updateProduct/:productId", verifyAdmin, upload, updateProduct);
+
+//For deleteProduct  - /api/v1/product/deleteProduct/:productId?id
+router.delete("/deleteProduct/:productId", verifyAdmin, deleteProduct);
 
 
-// => User routes
-// Route addProduct- /api/v1/product/addProduct
-router.route("/addProduct").post(upload,addProduct); 
+// => User routes 
 
-// Route getLatestProducts - /api/v1/product/getLatestProduct?sort=updateAt&order=desc&limit=10
-router.route("/getLatestProduct").get(latestProduct); 
+//For getLatestProducts - /api/v1/product/getLatestProduct?sort=updateAt&order=desc&limit=10
+router.route("/getLatestProduct").get(latestProduct);
 
-//To get all unique Categories  - /api/v1/product/categories
+//For get all unique Categories  - /api/v1/product/categories
 router.get("/categories", getAllCategories);
+
+//For get singleProduct  - /api/v1/product/getSingleProduct/:productId
+router.get("/getSingleProduct/:productId", getSingleProduct);
+
+//For search  - /api/v1/product/getSingleProduct/:productId
+router.get("/search", searchProduct);
 
 export default router;
